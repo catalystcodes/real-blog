9c12b9ee1b70e6ebb19b5da87454820e4aa191cf
mubby101 <muuh4mub@gmail.com>
2021-01-24 21:24:58 +0100

    update data base

---
+++ src/components/Hero/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../Logo'
+import Card from '../UI/Card'
+import './style.css'
+
+
+
+const Hero = (props) => {
+    return(
+        <div>
+             <Card>
+                 <Logo/>
+             </Card>
+        </div>
+    )
+}
+
+export default Hero
